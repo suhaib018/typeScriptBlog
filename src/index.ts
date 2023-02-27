2666a935d640f9d3bfce85e2d8c72b8a11a02b2f
suhaib018 <suhaibsaifan107@gmail.com>
2023-02-27 09:36:10 +0300

    more typescript specification and load image fixing

--- src/index.ts
+++ src/index.ts
@@ -21,28 +21,13 @@ const imageInput = <HTMLInputElement>document.querySelector("#file");
 const Heading = <HTMLInputElement>document.querySelector("#formatBlock");
 const color = <HTMLInputElement>document.querySelector("#color");
 const cardContainer = document.querySelector(".blog-card");
-const search = document.getElementById("search-blogs");
-const searchIcon = document.getElementById("search-icon");
 const cardContainerGrid =document.querySelector(".card-container");
-
-
-
-
-
-
-
-
-
-
-
-
 type blogObjet = {
   image?:any;
   name:string;
   title:string;
   body:string;
 };
-
 let blogs: blogObjet[] = [];
 let blogTitleChange:string ="";
 let blogAuthorChange:string ="";
@@ -50,37 +35,18 @@ let blogBodyChange:string ="";
 let blogImageChange:any;
 let personName:string ="";
 let searchedBlog :string = "";
-
 const myArray: blogObjet[] = JSON.parse(localStorage.getItem("myBlogs") || "[]");
-
-
-
-
-function searchBlogs() {
- 
-  const searchQuery = (<HTMLInputElement>document.getElementById("search-blogs")).value;
-  searchedBlog = searchQuery; 
-  console.log(`Searching for blogs with query: ${searchedBlog}`);
-}
-
-search?.addEventListener("change",searchBlogs);
-searchIcon?.addEventListener("click",function(){
-  
-  searchedBlog == ""  
-})
-
-
 
 myArray.forEach((blog) => {
   
-    let cardContainer = document.createElement("div"); // create a separate div for each card container
+    let cardContainer = document.createElement("div"); 
     let cardHeader = document.createElement("div");
     let cardBody = document.createElement("div");
     let image = document.createElement("img");
     let h4Heading = document.createElement("h4");
     let authorName = document.createElement("p");
   
-    cardContainer.className = "blog-card"; // add a class to the card container
+    cardContainer.className = "blog-card"; 
     cardHeader.className ="card-header";
     cardBody.className = "card-body"
     image.src =blog.image;
@@ -91,30 +57,14 @@ myArray.forEach((blog) => {
     cardBody.append(h4Heading);
     cardBody.append(authorName);
   
-    cardContainer.append(cardHeader); // append header and body to the card container
+    cardContainer.append(cardHeader); 
     cardContainer.append(cardBody);
   
-    cardContainerGrid?.append(cardContainer); // append the card container to the grid container
+    cardContainerGrid?.append(cardContainer); 
   
 })
 
-
-function loadImage(){
-  let file :any
-  if (imageInput && imageInput.files && imageInput.files.length > 0) {
-   file = imageInput.files[0];
-    // do something with file
-  } else {
-    console.log("No file selected");
-  }
-  if (previewImage){
-    previewImage.src = URL.createObjectURL(file);
-    blogImageChange =  previewImage.src;
-}
-}
-
-
-//BLOG EVENTS
+//############ BLOG EVENTS ############
 bold?.addEventListener("click",() => boldSelection());
 italick?.addEventListener("click",() => italicizeSelection());
 underLine?.addEventListener("click",() => underLinStyle());
@@ -128,16 +78,24 @@ orderList?.addEventListener("click",() => createOrderedList());
 unOrderList?.addEventListener("click",() => createUnOrderedList());
 Heading?.addEventListener("change",() => HeadingHandler());
 color?.addEventListener("change",() => colorHandler());
-imageInput?.addEventListener("change",loadImage);
+imageInput?.addEventListener("input",loadImage);
 
-
-
-
-
-
-function HeadingHandler() {
+//########## ACTIONS##########
+function loadImage() : void{
+  let file :any;
+  if (imageInput && imageInput.files && imageInput.files.length > 0) {
+  file = imageInput.files[0];
+  } else {
+    console.log("No file selected");
+  }
+  if (previewImage){
+    previewImage.src = URL.createObjectURL(file);
+    blogImageChange =  previewImage.src;
+  }
+}
+function HeadingHandler():void {
   // Get the selected text
-  const selection = window.getSelection();
+  const selection: Selection | null = window.getSelection();
   let level = Heading.value;
   // Check if there is any selected text
   if (selection && selection.rangeCount > 0) {
@@ -145,7 +103,7 @@ function HeadingHandler() {
     const range = selection.getRangeAt(0);
     // Get the desired heading level from the select element
     // Create a new heading element with the desired level
-    const heading = document.createElement(level);
+    const heading: HTMLElement = document.createElement(level);
     // Get the selected text and set it as the content of the heading element
     const selectedText = range.extractContents();
     heading.appendChild(selectedText);
@@ -153,41 +111,38 @@ function HeadingHandler() {
     range.insertNode(heading);
   }
 }
-
-function colorHandler() {
+function colorHandler():void {
   // Get the selected text
-  const selection = window.getSelection();
+  const selection: Selection | null = window.getSelection();
   // Check if there is any selected text
   if (selection && selection.rangeCount > 0) {
     // Get the selected range
-    const range = selection.getRangeAt(0);
+    const range:Range = selection.getRangeAt(0);
     // Create a new <span> element to wrap the selected text in
-    const span = document.createElement("span");
+    const span:HTMLSpanElement = document.createElement("span");
     span.style.color= color.value;
     // Wrap the selected text in the <span> element
     range.surroundContents(span);
   }
 }
-
-
-function createOrderedList() {
+function createOrderedList():void {
     // Get the selected text
-    const selection = window.getSelection();
+    const selection:Selection |null = window.getSelection();
     // Check if there is any selected text
     if (selection && selection.rangeCount > 0) {
       // Get the selected range
-      const range = selection.getRangeAt(0);
+      const range:Range = selection.getRangeAt(0);
       // Create a new <ol> element to wrap the selected text in
-      const ol = document.createElement("ol");
+      const ol:HTMLOListElement = document.createElement("ol");
       // Get the text content of the selection
-      const text = range.toString();
+      const text:string = range.toString();
       // Split the text content into an array of lines
       const lines = text.split("\n");
       // Create a new <li> element for each line of text
-      for (let i = 0; i < lines.length; i++) {
+      for (let i:number = 0; i < lines.length; i++) {
         const line = lines[i];
         // Create a new <li> element for this line of text
-        const li = document.createElement("li");
+        const li :HTMLLIElement = document.createElement("li");
         li.textContent = line;
         // Add the <li> element to the <ol> element
         ol.appendChild(li);
@@ -196,24 +151,22 @@ function createOrderedList() {
       range.deleteContents();
       range.insertNode(ol);
     }
-  }
-  
-
-  function createUnOrderedList() {
+}  
+function createUnOrderedList() {
     // Get the selected text
-    const selection = window.getSelection();
+    const selection:Selection | null = window.getSelection();
     // Check if there is any selected text
     if (selection && selection.rangeCount > 0) {
       // Get the selected range
-      const range = selection.getRangeAt(0);
+      const range:Range = selection.getRangeAt(0);
       // Create a new <ol> element to wrap the selected text in
-      const ul = document.createElement("ul");
+      const ul:HTMLUListElement = document.createElement("ul");
       // Get the text content of the selection
-      const text = range.toString();
+      const text:string = range.toString();
       // Split the text content into an array of lines
-      const lines = text.split("\n");
+      const lines:string[] = text.split("\n");
       // Create a new <li> element for each line of text
-      for (let i = 0; i < lines.length; i++) {
+      for (let i:number = 0; i < lines.length; i++) {
         const line = lines[i];
         // Create a new <li> element for this line of text
         const li = document.createElement("li");
@@ -225,162 +178,151 @@ function createOrderedList() {
       range.deleteContents();
       range.insertNode(ul);
     }
-  }
-  
-function justifyLeft() {
+} 
+function justifyLeft():void {
     // Get the selected text
-    const selection = window.getSelection();
+    const selection:Selection | null = window.getSelection();
     // Check if there is any selected text
     if (selection && selection.rangeCount > 0) {
       // Get the selected range
-      const range = selection.getRangeAt(0);
+      const range:Range = selection.getRangeAt(0);
       // Create a new <span> element to wrap the selected text in
-      const div = document.createElement("div");
+      const div:HTMLDivElement = document.createElement("div");
       div.style.textAlign= "left";
       // Wrap the selected text in the <span> element
       range.surroundContents(div);
     }
 }
-
-function justifyCenter() {
+function justifyCenter():void {
     // Get the selected text
-    const selection = window.getSelection();
+    const selection:Selection |null = window.getSelection();
     // Check if there is any selected text
     if (selection && selection.rangeCount > 0) {
       // Get the selected range
-      const range = selection.getRangeAt(0);
+      const range:Range = selection.getRangeAt(0);
       // Create a new <span> element to wrap the selected text in
-      const div = document.createElement("div");
+      const div:HTMLDivElement = document.createElement("div");
       div.style.textAlign= "center";
       // Wrap the selected text in the <span> element
       range.surroundContents(div);
     }
+}
+function justifyRight():void {
+  // Get the selected text
+  const selection:Selection | null = window.getSelection();
+  // Check if there is any selected text
+  if (selection && selection.rangeCount > 0) {
+    // Get the selected range
+    const range:Range = selection.getRangeAt(0);
+    // Create a new <span> element to wrap the selected text in
+    const div:HTMLDivElement = document.createElement("div");
+    div.style.textAlign= "right";
+    // Wrap the selected text in the <span> element
+    range.surroundContents(div);
   }
-
-  function justifyRight() {
+}
+function superScriptStyle():void {
     // Get the selected text
-    const selection = window.getSelection();
+    const selection:Selection | null = window.getSelection();
     // Check if there is any selected text
     if (selection && selection.rangeCount > 0) {
       // Get the selected range
-      const range = selection.getRangeAt(0);
+      const range:Range = selection.getRangeAt(0);
       // Create a new <span> element to wrap the selected text in
-      const div = document.createElement("div");
-      div.style.textAlign= "right";
-      // Wrap the selected text in the <span> element
-      range.surroundContents(div);
-    }
-  }
-
-function superScriptStyle() {
-    // Get the selected text
-    const selection = window.getSelection();
-    // Check if there is any selected text
-    if (selection && selection.rangeCount > 0) {
-      // Get the selected range
-      const range = selection.getRangeAt(0);
-      // Create a new <span> element to wrap the selected text in
-      const span = document.createElement("span");
+      const span:HTMLSpanElement = document.createElement("span");
       span.style.verticalAlign= "super";
       // Wrap the selected text in the <span> element
       range.surroundContents(span);
     }
-  }
-
-function subScriptStyle() {
+}
+function subScriptStyle():void {
     // Get the selected text
-    const selection = window.getSelection();
+    const selection:Selection | null = window.getSelection();
     // Check if there is any selected text
     if (selection && selection.rangeCount > 0) {
       // Get the selected range
-      const range = selection.getRangeAt(0);
+      const range:Range = selection.getRangeAt(0);
       // Create a new <span> element to wrap the selected text in
-      const span = document.createElement("span");
+      const span:HTMLSpanElement = document.createElement("span");
       span.style.verticalAlign= "sub";
       // Wrap the selected text in the <span> element
       range.surroundContents(span);
     }
-  }
-  
-
-function boldSelection() {
+}
+function boldSelection():void {
   // Get the selected text
-  const selection = window.getSelection();
+  const selection:Selection | null = window.getSelection();
   // Check if there is any selected text
   if (selection && selection.rangeCount > 0) {
     // Get the selected range
-    const range = selection.getRangeAt(0);
+    const range:Range = selection.getRangeAt(0);
     // Create a new <span> element to wrap the selected text in
-    const span = document.createElement("span");
+    const span:HTMLSpanElement = document.createElement("span");
     span.style.fontWeight = "bold";
     // Wrap the selected text in the <span> element
     range.surroundContents(span);
   }
 }
-
-
-function underLinStyle() {
+function underLinStyle():void {
     // Get the selected text
-    const selection = window.getSelection();
+    const selection:Selection | null = window.getSelection();
     // Check if there is any selected text
     if (selection && selection.rangeCount > 0) {
       // Get the selected range
-      const range = selection.getRangeAt(0);
+      const range:Range = selection.getRangeAt(0);
       // Create a new <span> element to wrap the selected text in
-      const span = document.createElement("span");
+      const span:HTMLSpanElement = document.createElement("span");
       span.style.textDecoration = "underline";
       // Wrap the selected text in the <span> element
       range.surroundContents(span);
     }
-  }
-
-  function lineThrough() {
-    // Get the selected text
-    const selection = window.getSelection();
-    // Check if there is any selected text
-    if (selection && selection.rangeCount > 0) {
-      // Get the selected range
-      const range = selection.getRangeAt(0);
-      // Create a new <span> element to wrap the selected text in
-      const span = document.createElement("span");
-      span.style.textDecoration = "line-through";
-      // Wrap the selected text in the <span> element
-      range.surroundContents(span);
-    }
-  }
-function italicizeSelection() {
+}
+function lineThrough():void {
   // Get the selected text
-  const selection = window.getSelection();
+  const selection:Selection | null = window.getSelection();
   // Check if there is any selected text
   if (selection && selection.rangeCount > 0) {
     // Get the selected range
-    const range = selection.getRangeAt(0);
+    const range:Range = selection.getRangeAt(0);
     // Create a new <span> element to wrap the selected text in
-    const span = document.createElement("span");
+    const span:HTMLSpanElement = document.createElement("span");
+    span.style.textDecoration = "line-through";
+    // Wrap the selected text in the <span> element
+    range.surroundContents(span);
+  }
+}
+function italicizeSelection():void {
+  // Get the selected text
+  const selection:Selection | null = window.getSelection();
+  // Check if there is any selected text
+  if (selection && selection.rangeCount > 0) {
+    // Get the selected range
+    const range:Range = selection.getRangeAt(0);
+    // Create a new <span> element to wrap the selected text in
+    const span:HTMLSpanElement = document.createElement("span");
     span.style.fontStyle = "italic";
     // Wrap the selected text in the <span> element
     range.surroundContents(span);
   }
 }
-Name?.addEventListener("change",function (){
+Name?.addEventListener("change",function ():void{
     personName = Name.value;
 })
-
-blogAuthor?.addEventListener("change",function (){
+blogAuthor?.addEventListener("change",function ():void{
   blogAuthorChange = blogAuthor.value;
 })
-
-blogTitle?.addEventListener("change",function (){
+blogTitle?.addEventListener("change",function ():void{
   blogTitleChange = blogTitle.value;
 })
-
-editor?.addEventListener("input",function (){
+editor?.addEventListener("input",function ():void{
     blogBodyChange = editor.innerText;
 })  
-
-postBtn?.addEventListener('submit',function(event){
+postBtn?.addEventListener('submit',function(event):void{
     event.preventDefault();
     blogs.push({image:blogImageChange,name:blogAuthorChange,title:blogTitleChange,body:blogBodyChange});
     localStorage.setItem("myBlogs",JSON.stringify(blogs));    
 })
-
+contactForm.addEventListener('submit',function (event):void{
+  event.preventDefault();
+  alert(`Hello ${personName} will we contact you soon`);
+})
